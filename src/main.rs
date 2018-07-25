86fe8bbbaa85018f4b83f1c5dbd530dff9edc049
Anders Musikka <anders@andersmusikka.se>
2018-07-25 11:46:42 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,57 @@
 #![feature(test)]
-
 extern crate scoped_pool;
 extern crate test;
 use scoped_pool::Pool;
 use test::{Bencher};
 
+
+/// This is a minimized program exhibiting a performance problem
+/// Why is this program twice as fast, when the number of threads is set to 1 instead of 2?
 #[bench]
 pub fn test_bench_alt(b: &mut Bencher) {
-	let parallellism = 2;
+	let parallellism = 1;
+	let data_size = 500_000;		
+	
 	let mut pool = Pool::new(parallellism);
-	let par = parallellism as usize;
 	
 	{
-
 		let mut data = Vec::new();
-		let data_size = 500_000;
 		for i in 0..data_size {
 			data.push(0);
 		}
 
-		let mut totvecs=Vec::<Vec<i32>>::new();
-		for i in 0..par {
-			let mut t = Vec::<i32>::with_capacity(data_size/par);
-			totvecs.push(t);
+		let mut output_data=Vec::<Vec<i32>>::new();
+		for i in 0..parallellism {
+			let mut t = Vec::<i32>::with_capacity(data_size/parallellism);
+			output_data.push(t);
 		}
 	    b.iter(move || {    	
 	    	
-	    	let dataref = &data;
-			for i in 0..par {
-				totvecs[i].clear();
+	    	for i in 0..parallellism {
+				output_data[i].clear();
 			}
 			{
 
-				let mut totvecref=&mut totvecs;
+				let mut output_data_ref=&mut output_data;
+                let dataref = &data;						
 				pool.scoped(move |scope| {
-					//let fidx=0;
-					let mut idx=0usize;
-					{
-						for sidx in totvecref.iter_mut() {						
-					        scope.execute(move || {					        
-					        	for item in &dataref[(idx*(data_size/par))..((idx+1)*(data_size/par))] {
-					        		sidx.push(*item);
-					        	}
-					        								        
-							});
-							idx+=1;
-					    }
+				
+					for (idx,output_data_bucket) in output_data_ref.iter_mut().enumerate() {						
+				        scope.execute(move || {					        
+				        	for item in &dataref[(idx*(data_size/parallellism))..((idx+1)*(data_size/parallellism))] { //Yes, this is a logic bug when parallellism does not evenely divide data_size. 
+				        		output_data_bucket.push(*item);
+				        	}
+				        								        
+						});
+				    }
 
-					}
+					
 				});
 			}
-			let mut totvecref2=&mut totvecs;
+			let mut output_data_ref=&mut output_data;
 			pool.scoped(move|scope|
-				for sub in totvecref2.iter_mut() {
-					//let sub:&Vec<i32> = sub;
+				for sub in output_data_ref.iter_mut() {
+
 			        scope.execute(move || {
 						for sublot in sub {
 
@@ -70,5 +68,5 @@ pub fn test_bench_alt(b: &mut Bencher) {
 
 
 fn main() {
-    println!("Hello, world!");
 }
+
